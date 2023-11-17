56c7dddb0157f62930278b8d8ea6fdfbc28015f8
carlosare76 <102261348+carlosare76@users.noreply.github.com>
2023-11-17 16:59:01 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoDAS.Models;
+
+namespace ProyectoDAS.Controllers
+{
+    public class AccesoController : Controller
+    {
+
+
+
+        // GET: Acceso
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoDAS.Models
+{
+    public class Usuario
+    {
+        public int IDusuario { get; set; }
+        public string Correo {  get; set; }
+        public string Clave { get; set; }
+        public string Rol {  get; set; }
+
+        public string confirmarClave { get; set; }
+    }
+}
